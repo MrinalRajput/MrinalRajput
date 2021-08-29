de5a26342aa40e6e950ecc8489ec2bc66d173c2f
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-08-29 12:43:28 +0530

    First Dc Bot

--- drum_bot.py
+++ drum_bot.py
@@ -101,7 +101,7 @@ async def warn(ctx, member:discord.Member, *, reason=None):
 @bot.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member:discord.Member, *, reason=None):
-    await member.kick(member)
+    await member.kick(reason=reason)
     await ctx.send(f"Kicked: {member.mention} has been Kicked from the Server by {ctx.author.mention}" if reason is None else f"Kicked: {member.mention} has been Kicked from the Server by {ctx.author.mention} \n\t With the Reason of :\t{reason}")
 
 @bot.command()
