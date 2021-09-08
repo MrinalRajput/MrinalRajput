ae1709eb2969218cad8c724b2a61054a810655d5
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-08 12:53:48 +0530

    First Dc Bot

--- drum_bot.py
+++ drum_bot.py
@@ -133,9 +133,9 @@ async def solve(ctx, num1:int, operation, num2:int):
             await ctx.send(f"{ctx.author.mention}  {num1} + {num2} = {num1 + num2}")
         elif operation == "-":
             await ctx.send(f"{ctx.author.mention}  {num1} - {operation}{num2} = {num1 - num2}")
-        elif operation == "*":
+        elif operation == "*" or operation.lower() == "x":
             await ctx.send(f"{ctx.author.mention}  {num1} x {num2} = {num1 * num2}")
-        elif operation == "/":
+        elif operation == "/" or operation == "÷":
             await ctx.send(f"{ctx.author.mention}  {num1} ÷ {num2} = {num1 / num2}")
     except Exception:
         embed = discord.Embed(title="Command : >solve", description=f"Usage : >solve [Number1] [Operation: +,-,*,/] [Number2]",color=embedTheme)
