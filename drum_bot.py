ffa30a97242ef6243b2a99688874d98b2a842176
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-09 14:54:06 +0530

    First Dc Bot

--- drum_bot.py
+++ drum_bot.py
@@ -126,7 +126,8 @@ class Giveaway():
     GiveawayChannel = None
 
     Participants = {
-
+        "already" : 243434,
+        "already2" : 232323,
     }
 
     @bot.command()
@@ -139,8 +140,8 @@ class Giveaway():
             # await asyncio.sleep(int(endtime))
             ParticipantsName = list(Participants.keys())
             embedStart = discord.Embed(title=f":loudspeaker: Giveaway has been Started :exclamation: :partying_face:\t ||{ctx.message.guild.default_role}||",color=embedTheme)
-            embedStart.add_field(name="Winner of the Giveaway",value=f"    ? \n\n Please Contact with The Giveaway Host For the Prize of this Giveaway",inline=False)
-            embedStart.add_field(name="Participants",value=f"{ParticipantsName}",inline= False)
+            embedStart.add_field(name="Winner of the Giveaway \t \t Time Left",value=f"\t ? \t\t\t {endtime}",inline=False)
+            embedStart.add_field(name="Participants",value=f"{ParticipantsName} \n\n Please Contact with The Giveaway Host For the Prize of this Giveaway",inline= False)
             StartAnnounce = await ctx.send(embed=embedStart)
             while -1 < endtime < endtime+1:
                 if GiveawayActive ==True:
