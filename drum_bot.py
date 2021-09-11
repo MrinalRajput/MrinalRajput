b01d06e62d22216fc8487ab4e43d03f0c8e2a7aa
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-11 15:09:40 +0530

    First Dc Bot

--- drum_bot.py
+++ drum_bot.py
@@ -191,7 +191,7 @@ async def setnick(ctx, member: Optional[discord.Member]=None, *, newname):
     await member.edit(nick=newname)
     embed = discord.Embed(title=f" _Done_ : {membername}'s Nickname has Changed to {newname} Successfully! ", color=embedTheme)
     embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested By {ctx.author.name}")
-    await ctx.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=15)
 
 @setnick.error
 async def setnick_error(error, ctx):
@@ -207,7 +207,7 @@ async def resetnick(ctx, member: Optional[discord.Member]=None):
     await member.edit(nick=None)
     embed = discord.Embed(title=f" _Done_ : {membername}'s Nickname has Removed Successfully! ", color=embedTheme)
     embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested By {ctx.author.name}")
-    await ctx.send(embed=embed)
+    await ctx.send(embed=embed, delete_after=15)
 
 @resetnick.error
 async def resetnick_error(error, ctx):
