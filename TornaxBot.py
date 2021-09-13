4d25472510e0133692372afae008278c2ff772bd
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-13 13:25:29 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -588,7 +588,8 @@ timerMsg = {}
 async def timerstart(ctx, seconds:int, *, reason: Optional[str]=None):
     global timer
     if ctx.guild.id not in timer:
-        timer[ctx.guild.id] = True
+        timer[ctx.guild.id] = False
+    if timer[ctx.guild.id] == False:
         timerMsg[ctx.guild.id] = await ctx.send(f"Timer has Started : `{seconds}`"if reason is None else f"{reason} `{seconds}`")
         while 0 < seconds < seconds+1:
             if timer[ctx.guild.id] == True:
