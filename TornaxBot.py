5f8861b12bd0d18f73354ffe11588a680c454e5c
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-22 09:45:19 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -807,6 +807,7 @@ async def on_message(message):
         afkdata[message.guild.id] = {}
     users = list(afkdata[message.guild.id].keys())
     for user in users:
+        print(user)
         if f"<@{user}>" in message.content:
             await message.channel.send(f"Afk: {message.author.mention} He is Currently Afk | Reason: {reasontopic[user.id]}")
 
