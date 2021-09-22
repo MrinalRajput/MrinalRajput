40b714a99ea204f4184a2f08070efb846fe514c1
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-22 09:59:41 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -862,6 +862,7 @@ async def vote(ctx):
     embed = discord.Embed(title="Vote For Tornax", color=embedTheme)
     embed.thumbnail(url=bot.user.avatar_url)
     embed.add_field(name="Click on Below Link", value="[Vote Now](https://top.gg/bot/832897602768076816/vote)")
+    embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested By {ctx.author.name}")
     await ctx.send(embed=embed)
 
 votehelp = ">vote"
