651622350d86c431d7aa1ad8b763446cf566b5c0
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-23 22:55:54 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -696,39 +696,39 @@ async def poll(ctx, question:Optional[str]=None, option1: Optional[str]=None, op
             if option1 is not None and option2 is not None:
                 if option10 is not None:
                     reactions = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣","9️⃣","🔟"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n 3️⃣ {option3}\n 4️⃣ {option4}\n 5️⃣ {option5}\n 6️⃣ {option6}\n 7️⃣ {option7}\n 8️⃣ {option8}\n 9️⃣ {option9}\n 🔟 {option10}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n 3️⃣ {option3}\n\n 4️⃣ {option4}\n\n 5️⃣ {option5}\n\n 6️⃣ {option6}\n\n 7️⃣ {option7}\n\n 8️⃣ {option8}\n\n 9️⃣ {option9}\n\n 🔟 {option10}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
                 elif option9 is not None:
                     reactions = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣","9️⃣"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n 3️⃣ {option3}\n 4️⃣ {option4}\n 5️⃣ {option5}\n 6️⃣ {option6}\n 7️⃣ {option7}\n 8️⃣ {option8}\n 9️⃣ {option9}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n 3️⃣ {option3}\n\n 4️⃣ {option4}\n\n 5️⃣ {option5}\n\n 6️⃣ {option6}\n\n 7️⃣ {option7}\n\n 8️⃣ {option8}\n\n 9️⃣ {option9}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
                 elif option8 is not None:
                     reactions = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n 3️⃣ {option3}\n 4️⃣ {option4}\n 5️⃣ {option5}\n 6️⃣ {option6}\n 7️⃣ {option7}\n 8️⃣ {option8}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n 3️⃣ {option3}\n\n 4️⃣ {option4}\n\n 5️⃣ {option5}\n\n 6️⃣ {option6}\n\n 7️⃣ {option7}\n\n 8️⃣ {option8}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
                 elif option7 is not None:
                     reactions = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n 3️⃣ {option3}\n 4️⃣ {option4}\n 5️⃣ {option5}\n 6️⃣ {option6}\n 7️⃣ {option7}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n 3️⃣ {option3}\n\n 4️⃣ {option4}\n\n 5️⃣ {option5}\n\n 6️⃣ {option6}\n\n 7️⃣ {option7}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
                 elif option6 is not None:
                     reactions = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n 3️⃣ {option3}\n 4️⃣ {option4}\n 5️⃣ {option5}\n 6️⃣ {option6}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n 3️⃣ {option3}\n\n 4️⃣ {option4}\n\n 5️⃣ {option5}\n\n 6️⃣ {option6}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
                 elif option5 is not None:
                     reactions = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n 3️⃣ {option3}\n 4️⃣ {option4}\n 5️⃣ {option5}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n 3️⃣ {option3}\n\n 4️⃣ {option4}\n\n 5️⃣ {option5}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
                 elif option4 is not None:
                     reactions = ["1️⃣","2️⃣","3️⃣","4️⃣"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n 3️⃣ {option3}\n 4️⃣ {option4}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n 3️⃣ {option3}\n\n 4️⃣ {option4}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
                 elif option3 is not None:
                     reactions = ["1️⃣","2️⃣","3️⃣"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n 3️⃣ {option3}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n 3️⃣ {option3}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
                 else:
                     reactions = ["1️⃣","2️⃣"]
-                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n 2️⃣ {option2}\n ", color=embedTheme)
+                    pollEmbed = discord.Embed(title=f"{question}  🤔", description=f"1️⃣ {option1}\n\n 2️⃣ {option2}\n\n ", color=embedTheme)
                     pollEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Poll by {ctx.author}")
 
                 pollMsg = await ctx.send(embed=pollEmbed)
