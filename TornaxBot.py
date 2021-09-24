7c5454c4b75060d690a34d7d7fc29f32945ddc40
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-24 20:03:58 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -1493,7 +1493,7 @@ helphelp = ">help [anycommand]"
 async def help(ctx, anycommand: Optional[str]=None):
     print(bot.all_commands.keys())
     totalCommands = len(bot.all_commands.keys())
-    print(bot.guilds, list(len(bot.guilds)))
+    print(bot.guilds, list(str(len(bot.guilds))))
     print(totalCommands)
     if anycommand is None:
         randomGreet = random.choice(["Hi","Hey","Hello"])
