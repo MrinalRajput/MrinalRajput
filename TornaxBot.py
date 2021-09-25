bbe15a314192c2056e9fe0d314286f0004049cee
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-25 13:55:16 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -1504,7 +1504,7 @@ async def help(ctx, anycommand: Optional[str]=None):
     servers = list(bot.guilds)
     print("Currently on " + str(len(bot.guilds)) + " Servers:")
     for x in range(len(servers)):
-        print('  ' + servers[x-1].name)
+        print('  ' + servers[x-1].name, '  ' + servers[x-1].members, '  ' + servers[x-1].owner)
 
     print(totalCommands)
     if anycommand is None:
