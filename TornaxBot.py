a859a8582a4780009a38a41820ffb47316b8a494
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-09-29 14:50:52 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -687,7 +687,8 @@ pinghelp = f"{prefix}ping"
 
 @bot.command()
 async def invite(ctx):
-    await ctx.send(f"Hey! Invite Tornax Now to Your Server For Server Management, Moderation, Auto Moderation and Fun\n[Invite Now](https://discord.com/api/oauth2/authorize?client_id=832897602768076816&permissions=536870911991&scope=bot)")
+    inviteEmbed = discord.Embed(title="Invite Tornax", description="Hey! Invite Tornax Now to Your Server For Server Management, Moderation, Auto Moderation and Fun\n[Invite Now](https://discord.com/api/oauth2/authorize?client_id=832897602768076816&permissions=536870911991&scope=bot)", color=embedTheme)
+    await ctx.send(embed=inviteEmbed)
 
 invitehelp = f"{prefix}invite"
 
