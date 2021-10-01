aab878459868828087416dee1fb036f5120fd967
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-10-01 13:35:58 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -25,8 +25,8 @@ async def load_prefix(bot, message):
         if not current_prefix:
             current_prefix = await bot.pg_con.execute("INSERT INTO prefixes(guild_id, prefix) VALUES($1,$2)", message.guild.id, DEFAULT_PREFIX)
 
-    return current_prefix
-    
+    return current_prefix(bot,message)
+
 bot = commands.Bot(command_prefix = commands.when_mentioned_or(load_prefix),case_insensitive=True ,help_command=None)
 
 TOKEN = "ODMyODk3NjAyNzY4MDc2ODE2.YHqeVg.yfzVgB8hHizDFH7hSMTORIv5weg"
