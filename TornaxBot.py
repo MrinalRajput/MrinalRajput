f8f9f962933933037f9c4d5a6c3fdd712252c8bb
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-10-02 23:00:45 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -12,6 +12,7 @@ import asyncpg
 from PIL import Image, ImageDraw
 from io import BytesIO
 import wikipedia
+from googlesearch import search
 
 from discord.ext.commands import has_permissions,has_role,MissingPermissions,MissingRole,CommandNotFound,CommandInvokeError, MissingAnyRole
 from discord.member import Member
@@ -803,7 +804,20 @@ async def wikipedia(ctx, *, search: Optional[str]=None):
         await ctx.reply(f"Something Went Wrong! We Didn't Found a Result for {search}")
 
 wikipediahelp = f"wikipedia <Search Topic>"
-    
+
+@bot.command()
+async def google(ctx, query):
+    result = search(query, num_results=10, lang="en", proxy=None)
+    result = " \n".join(result)
+    print(result)
+    googleEmbed = discord.Embed(title="Google Results", color=embedTheme)
+    googleEmbed.set_thumbnail(url="https://en.wikipedia.org/wiki/File:Google_%22G%22_Logo.svg")
+    googleEmbed.add_field(name=f"Results For {query}", value=f"{result}", inline=False)
+    googleEmbed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested By {ctx.author}")
+    await ctx.send(embed=googleEmbed)
+
+googlehelp = f"google <Search Topic>"
+
 @bot.command()
 async def time(ctx):
     currenttime = datetime.now()
@@ -1837,7 +1851,7 @@ async def help(ctx, anycommand: Optional[str]=None):
         myEmbed.add_field(name="Miscellaneous",value=" tell, poll, ping, afk, thought, vote, avatar, react, rule, rules, solve, time, timerstart, timerstop ", inline=False)
         myEmbed.add_field(name="Management",value=" addrole, removerole, clean, gstart, gstatus, gstop, gparticipate, gquit, setprefix, info, invite, about, support, join, leave, leaveserver, lock, slowmode, resetnick, setnick, unlock ", inline=False)
         myEmbed.add_field(name="Moderation",value=" kick, mute, warn, unmute, ban, unban ", inline=False)
-        myEmbed.add_field(name="Fun",value=" slap, kill, punch, wanted, tictactoe, tttstop, guess, mcserver, wikipedia \n----------------------\n", inline=False)
+        myEmbed.add_field(name="Fun",value=" slap, kill, punch, wanted, tictactoe, tttstop, guess, mcserver, wikipedia, google \n----------------------\n", inline=False)
         myEmbed.add_field(name="\n\n**Official Server**",value=f"----------------------\nJoin Our Official Server for More Commands and Help \n\n \t-> [Join Now](https://discord.gg/H3688EEpWr)\n----------------------\n\n > Server's Current Prefix is :   `{ctx.prefix}`\n > Command Usage Example :   `{ctx.prefix}info`\n\n----------------------", inline=False)
         myEmbed.add_field(name="Readme", value=f"`{ctx.prefix}help` Shows this Message, use `{ctx.prefix}help [command]` to get more information about that Command\n\n")
         myEmbed.set_footer(icon_url=bot.user.avatar_url,text=f"Made by {Creater}")
@@ -1873,6 +1887,7 @@ async def help(ctx, anycommand: Optional[str]=None):
         elif anycommand == "vote": content=votehelp
         elif anycommand == "support": content=supporthelp
         elif anycommand == "wikipedia": content=wikipediahelp
+        elif anycommand == "google": content=googlehelp
         elif anycommand == "join": content=joinhelp
         elif anycommand == "leave": content=leavehelp
         elif anycommand == "leaveserver": content=leaveserverhelp
