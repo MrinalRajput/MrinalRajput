3b0fc31e144254104edc90feb0ea59f6e4e0af5a
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-10-12 12:44:16 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -67,14 +67,17 @@ async def on_message(message):
         if message.content.lower().startswith(">maintenance"):
             channel = message.channel
             botstatus = discord.Status.dnd
+            await message.channel.send(f"Specify the Status of Tornax")
             def check(message):
                 return message.author.id == 758941956600102943 and message.channel == channel and message.content.lower().startswith(">status")
-            reply = await bot.wait_for("message", check=check, timeout=30)
+            reply = await bot.wait_for("message", check=check, timeout=50)
             botactivity = reply
+            await message.channel.send("Done ✅")
 
         elif message.content.lower().startswith(">statusnormal"):
             botstatus = discord.Status.online
             botactivity = "Server Members | >help for commands"
+            await message.channel.send("Done ✅")
 
 @bot.command()
 async def setprefix(ctx, *, newPrefix:Optional[str]=None):
