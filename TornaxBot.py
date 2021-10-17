b75840617b9f9a48b7af14753ee67ce5246de683
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-10-17 10:34:57 +0530

    First Dc Bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -2652,7 +2652,7 @@ async def on_message(message):
 @bot.listen()
 async def on_message(message):
     if message.content.lower().startswith(f"botservers"):
-        await message.channel.send("<:CuteJey:899153814056226836>")
+        await message.channel.send(":okey:")
         servers = list(bot.guilds)
         print(servers)
 
