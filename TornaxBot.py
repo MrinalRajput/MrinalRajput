b389b36722dfe1956010146a1dd3e345b5d6662f
Mrinal Rajput <mrinalrajput011@gmail.com>
2021-11-01 10:45:29 +0530

    bot

--- TornaxBot.py
+++ TornaxBot.py
@@ -10,6 +10,7 @@ import aiohttp
 from typing import Optional
 import json
 import time
+import pytz
 from discord.ext.commands.converter import EmojiConverter
 from discord.ext.commands.errors import BadArgument, BotMissingPermissions
 from discord.player import FFmpegPCMAudio
@@ -645,10 +646,15 @@ def getperiod(timing):
     alagalag = maintime.split()
     return alagalag
 
-def ending(seccs):
-    now = dt.datetime.now()
+def ending(ctx, seccs):
+    country = ctx.guild.region
+    getinfo = CountryInfo(country)
+    timezonee = str(pytz.country_timezones[str(getinfo.iso()['alpha2'])][0])
+    IST = pytz.timezone(timezonee)
+    
+    datetime_ist = dt.datetime.now(IST)
     delta = dt.timedelta(seconds = int(seccs))
-    t = now.time()
+    t = datetime_ist.time()
     k = (dt.datetime.combine(dt.date(1,2,3),t)+delta).time()
     lent = []
     for t in str(k):
@@ -688,7 +694,7 @@ async def gstart(ctx, gchannel: Optional[discord.TextChannel]=None, duration: Op
 
                 giveawayEmbed = discord.Embed(color=embedTheme)
                 giveawayEmbed.set_author(name=name.capitalize())
-                giveawayEmbed.add_field(name="Ending Time", value=ending(str(seccs)))
+                giveawayEmbed.add_field(name="Ending Time", value=ending(ctx, str(seccs)))
                 giveawayEmbed.add_field(name="Hosted By", value=ctx.author.mention)
                 giveawayEmbed.set_image(url="https://t4.ftcdn.net/jpg/04/61/96/99/240_F_461969925_Lu8i7asFdzjUnlo2kSEa6Yrdg3wBHHJ0.jpg")
                 giveawayEmbed.set_footer(text="React with 🎉 to Participate in the Giveaway")
